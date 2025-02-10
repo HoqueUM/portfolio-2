fdbbed410e10c26e4a45038cc560d332a3838e5a
Rahul Hoque <rhoque@umich.edu>
2025-02-10 15:01:04 -0500

    Update tasks

--- app/daily-tasks/page.tsx
+++ app/daily-tasks/page.tsx
@@ -13,11 +13,12 @@ export default function Home() {
       </nav> 
         <div className="flex font-mono">
             <div className="flex flex-col gap-4">
-            <div><a href="https://www.techinterviewhandbook.org/grind75/?weeks=26&hours=6" target="_blank" className="trace w-min">Leetcode</a> | <a href="/dsa-notes" target="_blank" className="trace w-min">Notes</a></div>
+            <div><a href="https://www.techinterviewhandbook.org/grind75/?weeks=26&hours=6" target="_blank" className="trace w-min">Leetcode</a> | <a href="/dsa-notes" target="_blank" className="trace w-min">Notes</a> | <a href="https://docs.google.com/spreadsheets/d/1ABpna_2y0Im1Trgk348JD7Pt4pzZ8fy4mD0zuxQFAM0/edit?gid=0#gid=0" target="_blank"></a>Spreadsheet</div>
             <a href="https://www.joinleland.com/library/a/top-free-resources-for-gmat-gre-practice" target="_blank" className="trace w-min">GMAT</a>
             <div><a href="https://www.linkedin.com/jobs/search/?currentJobId=4002613588&keywords=2026%20summer%20software%20engineer&origin=SWITCH_SEARCH_VERTICAL" target="_blank" className="trace w-min">Internships</a> and <a href="https://www.linkedin.com/jobs/search/?currentJobId=4112827224&geoId=103644278&keywords=new%20grand%20software%20engineer&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true" target=" _blank" className="trace w-min">Jobs</a></div>
             <a href="https://docs.google.com/document/d/1MOeS2PZ4F2gF3IpguIPBLX0nctTQjfLS1A0CoHiQZcs/edit?tab=t.0" target="_blank" className="trace w-min">Scripts</a>
             <div><a href="https://github.com/HoqueUM/deepseek-market" target="_blank" className="trace w-min">Projects</a> and <a href="https://www.codecademy.com/profiles/script0959629902" target="_blank" className="trace w-min">Javascript</a></div>
+            <a href="https://rahulhoque.substack.com/publish/home" target="_blank">Substack</a>
         </div>
         </div>
       </main>
