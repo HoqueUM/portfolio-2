7cf2f19932d0d86e1e8ba5171dbd4376915851e5
Rahul Hoque <rhoque@umich.edu>
2025-02-28 22:49:35 -0500

    update notes

--- app/dsa-notes/page.tsx
+++ app/dsa-notes/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { BsArrowReturnLeft } from "react-icons/bs";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function Home() {
   return (
@@ -8,10 +10,10 @@ export default function Home() {
       <main className="px-4 sm:px-8 pt-16 pb-24 max-w-4xl mx-auto">
       <nav className="top-0 pb-4 pt-4">
         <div className="flex">
-            <Link href="/daily-tasks"><div className="font-mono trace"><BsArrowReturnLeft /></div></Link>
+            <Link href="/daily-tasks"><div className="trace"><BsArrowReturnLeft /></div></Link>
         </div>
       </nav> 
-        <div className="flex font-mono">
+        <div className="flex">
             <div className="flex flex-col gap-4">
               <div>Sliding Window | Two Pointers | Fast & Slow Pointers</div>
               <div>Merge Intervals | Cyclic Sort | In-Place Reversal of a LinkedList</div>
@@ -20,7 +22,27 @@ export default function Home() {
               <div>K-way Merge | Knapsack | Topological Sort | Matrix Traversal</div>
             <br />
             <div>
-              <h1>Sliding Window</h1>
+              <h1 className="font-bold">Sliding Window</h1>
+              The sliding window technique is a method of extracting information from a subarray.
+              Depending on the problem, this could either require you to expand or contract the window.
+              A really good &apos;pesudocode&apos; I found <a href="https://medium.com/@timpark0807/leetcode-is-easy-sliding-window-c44c11cc33e1" target="blank" className="underline">here</a> gave this:
+              <SyntaxHighlighter language="python" style={vscDarkPlus}>
+              {
+                
+`def sliding_window(nums):
+  # Iterate over elements in our input, this is usually while left < len(nums)
+      # Expand the window
+      # Meet the condition to stop expansion
+          # Process the current window   
+          # Contract the window`}
+              </SyntaxHighlighter> 
+              The code filled in will depend on the problem (of course), but &apos;required&apos; variables are as follows:
+              <ul>
+                <li>left: the left boundary of our window</li>
+                <li>right: the right boundary of our window</li>
+                <li>result (or final return value): the result we are looking for</li>
+                <li>counter (or tracker): a counter that we can use to keep track of the window&apos;s state</li>
+              </ul>        
             </div>
         </div>
         </div>
