32ccf32b7f4218598ea0ab9e936c7a60724976be
Rahul Hoque <rhoque@umich.edu>
2025-05-08 10:57:52 -0400

    update project images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,8 +18,8 @@ export default function Projects() {
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               <Project name="NewDropz" image="/newdropz.png" description="A hub for new snack flavors." link="https://newdropz.com" />
-              <Project name="RotKings" image="/kalshi.avif" description="Track and analyze Rotten Tomatoes Scores for Kalshi trading." link="https://rotkings.com" />
-              <Project name="doomer.space" image="/hugging_face.avif" description="A collection of recent pessimistic headlines." link="https://doomer.space" />
+              <Project name="RotKings" image="/rotkings_dark.png" description="Track and analyze Rotten Tomatoes Scores for Kalshi trading." link="https://rotkings.com" />
+              <Project name="doomer.space" image="/doomer_dark.png" description="A collection of recent pessimistic headlines." link="https://doomer.space" />
           </div>
         </div>
       </main>
